0de41e44c15aa229cc19b5caa76220e2715dd72e
unknown <toxicshaon@gmail.com>
2023-06-10 16:59:23 +0600

    admin verify middleware created for verify admin

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
 
 
 
@@ -30,13 +45,27 @@ async function run() {
         await client.connect();
         const usersCollection = client.db('summerCamp').collection('users');
 
+        // jwt related api
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
+        // admin verify middleware 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user?.role !== 'admin') {
+                return res.status(401).send({ error: true, message: 'forbidden message' });
+            }
+            next()
+        }
 
 
         // users API
-
-
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
@@ -44,16 +73,49 @@ async function run() {
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            console.log(user)
+            // Set default role to "student"
+            user.role = 'student';
 
-            const query = { email: user.email }
-            const existingUser = await usersCollection.findOne(query)
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
             if (existingUser) {
-                return res.send({ message: 'user already exist' })
+                return res.send({ message: 'user already exists' });
             }
-            const result = await usersCollection.insertOne(user)
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+
+        // secure admin
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            if (req.decoded.email !== email) {
+                return res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { admin: user?.role === 'admin' }
             res.send(result)
         })
+
+        // secure teacher
+        app.get('/users/teacher/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            console.log(email)
+            if (req.decoded.email !== email) {
+                return res.send({ teacher: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { teacher: user?.role === 'teacher' }
+            res.send(result)
+        })
+
+
+
 
         // make admin api
         app.patch('/users/admin/:id', async (req, res) => {
